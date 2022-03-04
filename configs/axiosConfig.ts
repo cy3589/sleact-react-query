2f880f37ac09a0510def06d16d10eeeac76fcb49
정찬영 <jcy3589@gmail.com>
2022-03-04 09:19:01 +0000

    UPDATE::backendURL

--- configs/axiosConfig.ts
+++ configs/axiosConfig.ts
@@ -1,4 +1,4 @@
-const axiosBaseURL = 'http://api-sleact.cy3589.com';
+const axiosBaseURL = 'https://api-sleact.cy3589.com';
 const axiosBaseApiURL = `${axiosBaseURL}/api`;
 export { axiosBaseApiURL };
 export default axiosBaseURL;
